641426a3a241c6d8d86b09d7bed831ac7467728e
t0chka2020 <74494553+T0CHKA2@users.noreply.github.com>
2023-09-18 00:08:37 +0300

    Added comments

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ import machine, os
 from machine import Pin
 from time import sleep
 
+# Timer init
 timer_red = machine.Timer(1)
 timer_blue = machine.Timer(2)
 
+# Flag for some sort of timer
 flag_red = False
 flag_blue = False
 
+# Start timer
 time_blue = 0
 time_red = 0
 
+# Pins
 led_red = Pin(15, Pin.OUT)
 push_button_red = Pin(18, Pin.IN)
 
 led_blue = Pin(13, Pin.OUT)
 push_button_blue = Pin(12, Pin.IN)
 
+# Timer custom callbacks
 def plustimered():
   global time_red
   time_red = time_red + 1
@@ -25,10 +30,12 @@ def plustimeblue():
   global time_blue
   time_blue = time_blue + 1
 
+# Turn off mechanics (fully)
 def turnoff():
   flag_stop = True
 
 flag_stop = False
+
 
 while not flag_stop:
   logic_state_blue = push_button_blue.value()
@@ -49,13 +56,13 @@ while not flag_stop:
     flag_red = False
     flag_blue = False
     turnoff()
-    sleep(10)
-    print(1)
+    sleep(10) # Debug feature
+    print(1) # Debug feature
     f = open('timestamp.txt', 'a')
     f.write("Time red (s) = " + str(round(time_red/150)) + '\n' + "Time blue (s) = " + str(round(time_blue/150)))
     f.close()
-    print(f.read())
-    sleep(60)
+    print(f.read()) # Debug feature
+    sleep(60) # Debug feature
   elif logic_state_red and not led_state_red:
     led_red.on()
     led_blue.off()
@@ -69,7 +76,7 @@ while not flag_stop:
     flag_red = False
     flag_blue = True
 
-  # Time report
+  # Time report, yeah, this will spam
   if flag_stop is True:
     pass
   elif flag_blue or flag_red:
